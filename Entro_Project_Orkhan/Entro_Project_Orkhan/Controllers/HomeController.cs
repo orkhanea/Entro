6025792d3c89307a53ddd8874e326512ad50950e
Orkhan Amirli <orkhanea@code.edu.az>
2021-12-04 18:08:20 +0400

    Entro_first

---
+++ Entro_Project_Orkhan/Entro_Project_Orkhan/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace Entro_Project_Orkhan.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
